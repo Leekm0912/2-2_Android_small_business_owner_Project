0361159bfe4b934a12d5cec7d09eec6966d3a86f
Byeong-Gwon <qrzz741@naver.com>
2020-12-02 12:09:07 +0900

    게시판에 라디오 버튼이 데이터베이스 넘어갈때 값을 주는지 테스트가 필요함

--- app/src/main/java/net/ddns/leekm/eco_design_app/borad.java
+++ app/src/main/java/net/ddns/leekm/eco_design_app/borad.java
@@ -8,18 +8,24 @@ import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class borad extends AppCompatActivity {
     Button submit;
     RadioGroup rg;
-
+    TextInputLayout title;
+    TextInputLayout story;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borad);
+        //db에 담을 제목, 내용
+        title = findViewById(R.id.title);
+        story = findViewById(R.id.story);
 
         submit = findViewById(R.id.submit);
         rg = (RadioGroup)findViewById(R.id.bookType);
-        
+
         submit.setOnClickListener((v)->{
             Intent intent;
             Toast.makeText(this,"등록되었습니다.",Toast.LENGTH_SHORT).show();
